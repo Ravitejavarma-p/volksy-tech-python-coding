a32927aa0ddd7e468e7fddef2d8a3638c67b082f
Ravitejavarma-p <ravitejav@brightcomgroup.com>
2022-12-26 06:56:41 +0000

    7-task

--- python-hello_world/3-last_digit.py
+++ python-hello_world/3-last_digit.py
@@ -9,9 +9,9 @@ else:
     if last > 5:
         line = ("Last digit of " + str(number) +
                 " is " + str(last) + " and is greater" +
-                "than 5")
+                " than 5")
     elif last < 5:
         line = ("Last digit of " + str(number) +
                 " is " + str(last) + " and is less" +
-                "than 6 and not 0")
+                " than 6 and not 0")
 print(line)
