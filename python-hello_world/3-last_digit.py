0ae2b2f0f2ed9bf1b2ecc3ba875eec56cb689044
Ravitejavarma-p <ravitejav@brightcomgroup.com>
2022-12-26 07:00:35 +0000

    7-task

--- python-hello_world/3-last_digit.py
+++ python-hello_world/3-last_digit.py
@@ -5,12 +5,12 @@ last = number % 10
 if last == 0:
     line = ("Last digit of " + str(number) +
             " is " + str(last) + " and is 0")
-else:
+elif:
     if last > 5:
         line = ("Last digit of " + str(number) +
                 " is " + str(last) + " and is greater" +
                 " than 5")
-    elif last < 5:
+    else last < 5:
         line = ("Last digit of " + str(number) +
                 " is " + str(last) + " and is less" +
                 " than 6 and not 0")
